8d4e7cee67bf3361b76eacabe6ba5f9134a627bb
Jack Paw <42397276+unixbadass@users.noreply.github.com>
2018-08-16 15:19:08 +0200

    Create post.php

---
+++ post.php
@@ -0,0 +1,13 @@
+<?PHP
+function post_api($url, $postfields) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $postfields);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        $result = curl_exec($ch);
+        return $result;
+}
+     
